fa64edb6dadcd08a259d21970171b58276f8d5cf
MohamedMokhtar18 <welleamy@gmail.com>
2022-09-07 00:14:48 +0300

    -added mpi locks

--- Mstest.c
+++ Mstest.c
@@ -4,7 +4,7 @@
 
 int main(int argc, char *argv[])
 {
-    int my_rank_world, size_world;
+    int size_world;
     int my_rank_sm, size_sm;
     int rcv_buf, sum, i;
     int *rcv_buf_ptr;
@@ -12,8 +12,8 @@ int main(int argc, char *argv[])
     MPI_Comm comm_sm;
 
     MPI_Win win;
-
     MPI_Init(&argc, &argv);
+    // splitting memory into islands
     MPI_Comm_split_type(MPI_COMM_WORLD, MPI_COMM_TYPE_SHARED, 0, MPI_INFO_NULL, &comm_sm);
     MPI_Comm_rank(comm_sm, &my_rank_sm);
     MPI_Comm_size(comm_sm, &size_sm);
@@ -30,19 +30,17 @@ int main(int argc, char *argv[])
         }
     }
     MPI_Win_allocate_shared(sizeof(int), sizeof(int), MPI_INFO_NULL, comm_sm, &rcv_buf_ptr, &win);
+    MPI_Win_lock_all(0, win);
     double start = MPI_Wtime();
 
-    sum = 100;
+    sum = 100000000;
     snd_buf = sum;
 #pragma omp parallel for
 
-    for (i = 0; i < size_sm; i++)
+    for (i = 0; i < 4; i++)
     {
 
-        /* *** The following 3 lines shall be substituted by
-         *** 1-sided communication and synchronization    */
         MPI_Win_fence(0, win);
-        // MPI_Put(&snd_buf, 1, MPI_INT, right, (MPI_Aint)0, 1, MPI_INT, win);
         *(rcv_buf_ptr) = snd_buf; // to store into right neighbor's rcv_buf
         MPI_Win_fence(0, win);
 
@@ -63,11 +61,11 @@ int main(int argc, char *argv[])
             sum = *rcv_buf_ptr;
         }
     }
-
+    MPI_Win_sync(win);
     printf("PE%i:\t Data = %i\n", my_rank_sm, sum);
     double end = MPI_Wtime();
     printf("The process took %fseconds to run\n", end - start);
-
+    MPI_Win_unlock_all(win);
     MPI_Win_free(&win);
     MPI_Finalize();
 }
