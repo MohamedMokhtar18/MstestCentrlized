cc3c0adb6687e725f35e9fb27bce268f03fee69e
MohamedMokhtar18 <welleamy@gmail.com>
2022-09-18 04:31:48 +0300

    -update fixing sync messing data

--- Mstest.c
+++ Mstest.c
@@ -5,15 +5,17 @@
 int main(int argc, char *argv[])
 {
     int size_world;
+    int rank_world;
     int my_rank_sm, size_sm;
     int rcv_buf, sum, i;
     int *rcv_buf_ptr;
     int snd_buf, provided;
-    ;
-    MPI_Comm comm_sm;
 
+    MPI_Comm comm_sm;
     MPI_Win win;
     MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided);
+    MPI_Comm_size(MPI_COMM_WORLD, &size_world);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank_world);
     // MPI_Init(&argc, &argv);
     // splitting memory into islands
     MPI_Comm_split_type(MPI_COMM_WORLD, MPI_COMM_TYPE_SHARED, 0, MPI_INFO_NULL, &comm_sm);
@@ -24,6 +26,7 @@ int main(int argc, char *argv[])
         if (size_sm == size_world)
         {
             printf("MPI_COMM_WORLD consists of only one shared memory region\n");
+            printf("MPI_COMM_WORLD is split into %d \n", size_sm);
         }
         else
         {
@@ -38,36 +41,35 @@ int main(int argc, char *argv[])
     snd_buf = sum;
     MPI_Win_lock_all(0, win);
 
-    for (i = 0; i < 4; i++)
+    for (i = 0; i < size_world; i++)
     {
-
-        *(rcv_buf_ptr) = snd_buf; // to store into right neighbor's rcv_buf
-
-        snd_buf = *rcv_buf_ptr;
-
-        sum = *rcv_buf_ptr;
-    }
-    MPI_Win_unlock_all(win);
-
-    if (my_rank_sm > 4)
-    {
-
-        for (i = 5; i > size_sm && size_sm < size_world; i++)
+        if (i == my_rank_sm)
         {
-            if (my_rank_sm > i)
+            *(rcv_buf_ptr) = snd_buf; // to store into right neighbor's rcv_buf
+
+            snd_buf = *rcv_buf_ptr;
+
+            sum = *rcv_buf_ptr;
+        }
+        else if (my_rank_sm >= 4)
+        {
+
+            if (i == my_rank_sm)
             {
-                MPI_Win_lock(MPI_LOCK_EXCLUSIVE, (i - 4), 0, win);
-                MPI_Get(&rcv_buf, 1, MPI_INT, (i - 4), (MPI_Aint)0, 1, MPI_INT, win);
+                // MPI_Rget(rcv_buf_ptr, 1, MPI_INT, (1 - 4), (MPI_Aint)0, 1, MPI_INT, win, &req);
+                // MPI_Wait(&req, &status);
+                MPI_Get(rcv_buf_ptr, 1, MPI_INT, (i - 4), (MPI_Aint)0, 1, MPI_INT, win);
                 // MPI_Put(&snd_buf, 1, MPI_INT, right, (MPI_Aint)0, 1, MPI_INT, win);
-                MPI_Win_unlock((i - 4), win);
+
                 sum = *rcv_buf_ptr;
             }
         }
     }
+    MPI_Win_unlock_all(win);
+
     MPI_Win_sync(win);
     printf("PE%i:\t Data = %i\n", my_rank_sm, sum);
     double end = MPI_Wtime();
     printf("The process took %fseconds to run\n", end - start);
-    MPI_Win_free(&win);
     MPI_Finalize();
 }
